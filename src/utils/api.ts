69d8c36e5364205e576b6b398e9a78f00c056d16
Ronald Tomaz <ronald@avanz.com.br>
2025-04-23 12:15:07 -0300

    fix: update default revalidation time to 120 seconds

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,3 @@
 export const API_BASE_URL: string = process.env.API_URL || ''
 export const REVALIDATE_TIME =
-  process.env.NEXT_PUBLIC_REVALIDATE_TIME_IN_SECONDS || '300'
+  process.env.NEXT_PUBLIC_REVALIDATE_TIME_IN_SECONDS || '120'
